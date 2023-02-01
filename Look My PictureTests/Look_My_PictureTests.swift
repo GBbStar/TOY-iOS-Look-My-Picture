6a274b10948574a2dac76f547ec5f7bd119dee27
GyuBin Hwang <spiraldailydev@gmail.com>
2023-02-01 21:35:30 +0900

    study ios project commit

---
+++ Look My PictureTests/Look_My_PictureTests.swift
@@ -0,0 +1,33 @@
+//
+//  Look_My_PictureTests.swift
+//  Look My PictureTests
+//
+//  Created by gbstar on 2023/02/01.
+//
+
+import XCTest
+@testable import Look_My_Picture
+
+class Look_My_PictureTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
